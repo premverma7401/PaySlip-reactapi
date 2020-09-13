624747090ab77f453fd97d00143eee66bb31df47
Rajan verma <rajanverma5410@gmail.com>
2020-09-13 10:01:46 +0530

    updated content of infomation

--- payslip-ui/src/components/CreatePayslip.js
+++ payslip-ui/src/components/CreatePayslip.js
@@ -4,11 +4,18 @@ import Navbar from './Navbar';
 const CreatePayslip = () => {
   return (
     <div>
-      <Navbar title="Create Payslips" />
-      <div class="payslip-inputs">
+      <Navbar title="Generate Payslips" />
+        <div className="main">
+        <div class="payslip-inputs">
             <span>
-                <input type="text" placeholder="Employee Name" />
-                <input type="text" placeholder="Employee ID" />
+                <label for="employee-list">Employee Name </label>
+                <select Name="employee-list" id="employee-list">
+                <option>Name1</option>
+                <option>Name2</option>
+                <option>Name3</option>
+                <option>Name4</option>
+                </select>
+                <input type="number" placeholder="NO. of hours worked" />
             </span>
             <span>
                 <h1>PAYSLIP</h1>
@@ -16,27 +23,26 @@ const CreatePayslip = () => {
         </div> 
         <div class="record">
             <table>
-                    <tr>
-                        <th>Earnings</th>
-                        <th>Hours</th>
-                        <th>Rate</th>
-                    </tr>
-                    
-                    <tr>
-                        <td>Contract Work</td>
-                        <td>8</td>
-                        <td>10</td>
-                    </tr>
-                    <tr>
-                        <td>Overtime Work</td>
-                        <td>8</td>
-                        <td>10</td>
-                    </tr>
-                    <tr>
-                        <th>TOTAL</th>
-                        <th>16</th>
-                        <th>20</th>
-                    </tr>
+                <tr>
+                    <th>Earnings</th>
+                    <th>Hours</th>
+                    <th>Rate</th>
+                </tr>
+                <tr>
+                    <td>Contract Work</td>
+                    <td>8</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td>Overtime Work</td>
+                    <td>8</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <th>TOTAL</th>
+                    <th>16</th>
+                    <th>20</th>
+                </tr>
             </table>
             <table class="deduction">
                 <tr>
@@ -65,7 +71,8 @@ const CreatePayslip = () => {
                     <th>3000</th>
                 </tr>
             </table>
-        </div>    
+        </div>  
+        </div>  
     </div>
   );
 };

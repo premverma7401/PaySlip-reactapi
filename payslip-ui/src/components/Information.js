8bbfb5ea7f79b61aeac6779430d86b473baa8214
Prem <premverma7401@gmail.com>
2020-09-13 17:57:16 +1200

    rework-information

--- payslip-ui/src/components/Information.js
+++ payslip-ui/src/components/Information.js
@@ -1,124 +1,55 @@
 import React from 'react';
 import Navbar from './Navbar';
+import '../styles/information.css';
 
 const Information = () => {
   return (
     <div>
-        <Navbar title="Company Details" />
-        <div className="main">
-            <div className="company-profile">
-                <div className="left">
-                    <div className="employee-card">
-                        <div class="profile-card">
-                                <div class="card-image">
-                                    <img src="https://image.ibb.co/c9rY6J/profile02.jpg" alt="profile two"/>
-                                </div>
-                                <div class="emp-name-title">
-                                    <h2>Mary Jane
-                                        <br/>
-                                        <span>UX Designer</span>
-                                        <br/>
-                                        <span>Total Working Hours</span>
-                                        <br/>
-                                        <span>Total Earning</span>
-                                    </h2>
-                                </div>
-                            </div>
-                        <div class="profile-card">
-                                <div class="card-image">
-                                    <img src="https://image.ibb.co/c9rY6J/profile02.jpg" alt="profile two"/>
-                                </div>
-                                <div class="emp-name-title">
-                                    <h2>Mary Jane
-                                        <br/>
-                                        <span>UI Designer</span>
-                                        <br/>
-                                        <span>Total Working Hours</span>
-                                        <br/>
-                                        <span>Total Earning</span>
-                                    </h2>
-                                </div>
-                            </div>
-                        <div class="profile-card">
-                                <div class="card-image">
-                                    <img src="https://image.ibb.co/c9rY6J/profile02.jpg" alt="profile two"/>
-                                </div>
-                                <div class="emp-name-title">
-                                    <h2>Mary Jane
-                                        <br/>
-                                        <span>UI Designer</span>
-                                        <br/>
-                                        <span>Total Working Hours</span>
-                                        <br/>
-                                        <span>Total Earning</span>
-                                    </h2>
-                                </div>
-                            </div>
-                        <div class="profile-card">
-                            <div class="card-image">
-                                <img src="https://image.ibb.co/c9rY6J/profile02.jpg" alt="profile two"/>
-                            </div>
-                            <div class="emp-name-title">
-                                <h2>Mary Jane
-                                    <br/>
-                                    <span>UI Designer</span>
-                                    <br/>
-                                    <span>Total Working Hours</span>
-                                    <br/>
-                                    <span>Total Earning</span>
-                                </h2>
-                            </div>
-                        </div>
-                        <div class="profile-card">
-                            <div class="card-image">
-                                <img src="https://image.ibb.co/c9rY6J/profile02.jpg" alt="profile two"/>
-                            </div>
-                            <div class="emp-name-title">
-                                <h2>Mary Jane
-                                    <br/>
-                                    <span>UI Designer</span>
-                                    <br/>
-                                    <span>Total Working Hours</span>
-                                    <br/>
-                                    <span>Total Earning</span>
-                                </h2>
-                            </div>
-                        </div>
-                        <div class="profile-card">
-                            <div class="card-image">
-                                <img src="https://image.ibb.co/c9rY6J/profile02.jpg" alt="profile two"/>
-                            </div>
-                            <div class="emp-name-title">
-                                <h2>Mary Jane
-                                    <br/>
-                                    <span>UI Designer</span>
-                                    <br/>
-                                    <span>Total Working Hours</span>
-                                    <br/>
-                                    <span>Total Earning</span>
-                                </h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="right">
-                    <div>
-                        <h2>insights</h2>
-                        <span class="stats">
-                            <p>Total Employees</p>
-                            <p>Software Developers</p>
-                            <p>Team Lead</p>
-                            <p>Bussiness Anlyst</p>
-                            <p>Software Tester</p>
-                        </span>
-                    </div>
-                    <div>
-                        <span></span>
-                    </div>
-                </div>
-            </div>
-        </div> 
-    </div>      
+      <Navbar title="Company Details" />
+      <div className="info-parent main">
+        <div className="card-group">
+          <div className="user-summary-card">
+            <img
+              src="https://image.ibb.co/c9rY6J/profile02.jpg"
+              alt="profile two"
+            />
+            <button>View Info</button>
+          </div>
+          <div className="user-summary-card">
+            <img
+              src="https://image.ibb.co/c9rY6J/profile02.jpg"
+              alt="profile two"
+            />
+            <button>View Info</button>
+          </div>
+          <div className="user-summary-card">
+            <img
+              src="https://image.ibb.co/c9rY6J/profile02.jpg"
+              alt="profile two"
+            />
+            <button>View Info</button>
+          </div>
+          <div className="user-summary-card">
+            <img
+              src="https://image.ibb.co/c9rY6J/profile02.jpg"
+              alt="profile two"
+            />
+            <button>View Info</button>
+          </div>
+          <div className="user-summary-card">
+            <img
+              src="https://image.ibb.co/c9rY6J/profile02.jpg"
+              alt="profile two"
+            />
+            <button>View Info</button>
+          </div>
+        </div>
+        <div className="right-stats">
+          <div className="info-stats"></div>
+          <div className="info-pie"></div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- payslip-ui/src/components/ViewEmployees.js
+++ payslip-ui/src/components/ViewEmployees.js
@@ -1,59 +1,57 @@
 import React from 'react';
 import Navbar from './Navbar';
-import { getByDisplayValue } from '@testing-library/react';
-import CreateEmployee from './CreateEmployee';
-
+import '../styles/viewEmployee.css';
 const ViewEmployees = () => {
   return (
     <div>
       <Navbar title="View Employee" />
-    <div className="main">
+      <div className="main">
         <div className="emp-details">
+          <div>
+            <p>GENERAL DETAILS</p>
             <div>
-                <p>GENERAL DETAILS</p> 
-                <div>
-                    <span>User name</span>
-                    <span>Full name</span>
-                    <span>Email</span>
-                </div>
-                <div>
-                    <span>Contact No.</span>
-                    <span>Department</span>
-                    <span>Designantion</span>
-                </div>
+              <span>User name</span>
+              <span>Full name</span>
+              <span>Email</span>
             </div>
             <div>
-                <p>PERSONAL DETAILS</p> 
-                <div>
-                    <span>Full Name</span>
-                    <span>E-Mail ID</span>
-                    <span>IRD</span>
-                    <span>Date Of Birth</span>
-                </div>
-                <div>
-                    <span>UserName</span>
-                    <span>Contact No.</span>
-                    <span>Address</span>
-                    <span>Age</span>
-                </div>
+              <span>Contact No.</span>
+              <span>Department</span>
+              <span>Designantion</span>
+            </div>
+          </div>
+          <div>
+            <p>PERSONAL DETAILS</p>
+            <div>
+              <span>Full Name</span>
+              <span>E-Mail ID</span>
+              <span>IRD</span>
+              <span>Date Of Birth</span>
             </div>
             <div>
-                <p>CONTRACT DETAILS</p>
-                <div>
-                    <span>Contract Hours</span>
-                    <span>Pay Per Hour</span>
-                    <span>Union</span>
-                    <span>Date Of Contract</span>
-                </div>
-                <div>
-                    <span>Contract type</span>
-                    <span>Overtime Rate</span>
-                    <span>Kiwi Saver</span>
-                    <span>Pay Record</span>
-                </div>
+              <span>UserName</span>
+              <span>Contact No.</span>
+              <span>Address</span>
+              <span>Age</span>
             </div>
-        </div >         
-    </div>
+          </div>
+          <div>
+            <p>CONTRACT DETAILS</p>
+            <div>
+              <span>Contract Hours</span>
+              <span>Pay Per Hour</span>
+              <span>Union</span>
+              <span>Date Of Contract</span>
+            </div>
+            <div>
+              <span>Contract type</span>
+              <span>Overtime Rate</span>
+              <span>Kiwi Saver</span>
+              <span>Pay Record</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
